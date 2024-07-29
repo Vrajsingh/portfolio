bef0f3865fcba6bf8b12bb62981469191c7ee09e
Vrajsingh <vishalrajput97084@gmail.com>
2024-07-30 00:37:14 +0530

    created portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <br />
+              <p>
+                I'm a frontend react developer with experience in building responsive
+                and optimized sites 
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <br />
+              <p>
+                I have experience in developing fast and optimised back-end systems
+                and APIs using SpringBoot application
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Database Management</h3>
+              <br />
+              <p>
+              I have experience working with PostgreSQL and MongoDB databases while handling client projects.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
